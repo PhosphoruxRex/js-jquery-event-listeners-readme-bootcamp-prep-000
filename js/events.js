091197a33da667bd052eb6f3e2e9ca06e0f6bf79
Lung Chen <lungheichen@gmail.com>
2018-06-29 18:22:28 +0000

    automatically backed up by learn

--- js/events.js
+++ js/events.js
@@ -1,29 +1,29 @@
 //define functions here
 function getIt() {
-  $('p').on('click', function(){
-    alert("Hey!");
-  });
+  $('p').on('click', function() {
+    alert('hey!')
+  })
 }
 
 function frameIt() {
-  $('img').on('load', function(){
-    $("img").addClass("tasty");
-  });
+  $('img').on('load', function() {
+    $('img').addClass('tasty')
+  })
 }
 
 function pressIt() {
-// $('#typing').on('keydown', function(key){
-  $('input').on('keydown', function(key){
-    if (key.which == 71) {
-      alert('G has been pressed');
+  $('#typing').on('keydown', function(e) {
+    // 'g' corresponds to 71
+    if (e.which === 71) {
+      alert("You pressed 'g'!")
     }
-  });
+  })
 }
 
 function submitIt() {
-  $("form").on("submit", function() {
-    alert('Your form is going to be submitted now.');
-  });
+  $('form').on('submit', function(e) {
+    alert('Your form is going to be submitted now.')
+  })
 }
 
 
